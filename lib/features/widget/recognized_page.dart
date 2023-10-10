a005bc50be509776c3112e3925048d7836a93a9e
aja237 <azumbij@gmail.com>
2023-10-10 13:43:10 +0000

    Oct 10, 2023, 2:43 PM

--- lib/features/widget/recognized_page.dart
+++ lib/features/widget/recognized_page.dart
@@ -13,7 +13,6 @@ class RecognizePage extends StatefulWidget {
 class _RecognizePageState extends State<RecognizePage> {
   bool _isBusy = false;
   final TextEditingController _controller = TextEditingController();
-  // late InputImage inputImage;
   List<Face> _faces = [];
 
   @override
@@ -34,38 +33,38 @@ class _RecognizePageState extends State<RecognizePage> {
           ),
           body: _isBusy
               ? const Center(child: CircularProgressIndicator())
-              : Expanded(
-                  // padding: const EdgeInsets.all(20),
-                  child: Column(
-                children: [
-                  Container(
-                    height: 100,
-                    child: TextFormField(
-                      controller: _controller,
-                      maxLines: height,
-                      decoration: const InputDecoration(
-                          hintText: "Text goes here..."),
-                    ),
-                  ),
-                  Stack(
+              : SingleChildScrollView(
+                  child: Expanded(
+                      // padding: const EdgeInsets.all(20),
+                      child: Column(
                     children: [
-                      Positioned(
-                        left: 0,
-                        top: 0,
-                        child: Container(
-                            decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage(widget.path!),
-                              fit: BoxFit.cover),
-                        )),
-                      )
+                      Container(
+                        height: 100,
+                        child: TextFormField(
+                          controller: _controller,
+                          maxLines: height,
+                          decoration: const InputDecoration(
+                              hintText: "Text goes here..."),
+                        ),
+                      ),
+                      Stack(
+                        children: [
+                          Positioned(
+                            child: Container(
+                                decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(widget.path!),
+                                  fit: BoxFit.cover),
+                            )),
+                          )
 
-                      // for (Face face in _faces)
-                      //   _drawFaceRect(face, context),
+                          // for (Face face in _faces)
+                          //   _drawFaceRect(face, context),
+                        ],
+                      ),
                     ],
-                  ),
-                ],
-              ))),
+                  )),
+                )),
     );
   }
 
