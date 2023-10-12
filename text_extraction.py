04047d22a967a5c6f86c2a0f849b166c36fc6a2d
aja237 <azumbij@gmail.com>
2023-10-12 09:43:24 +0000

    Oct 12, 2023, 10:43 AM

--- text_extraction.py
+++ text_extraction.py
@@ -18,7 +18,7 @@ class TextExtract:
         self.string = {}  # non-numeric values extracted
         self.extract()  # method for extraction
 
-    def numeric_handler(self):  # sourcery skip: raise-specific-error
+    def numeric_handler(self):
         """ 
         method responsible for handling numeric values found on the image.
 
